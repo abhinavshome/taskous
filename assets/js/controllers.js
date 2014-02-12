90e62382943194039091ccde303379804f456817
Kumar Abhinav <kabhinav.iitg@gmail.com>
2014-02-12 11:09:13 +0530

    basic done

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,64 +1,89 @@
-angular.module("taskous.controllers", [])
-    .controller('TopMenuController', function ($scope, $rootScope, Project) {
-        $scope.projects = Project.query(function () {
-            $rootScope.currentProject = $scope.projects[0];
-        });
-        $scope.selectProject = function (project) {
-            $rootScope.currentProject = project;
-            console.log('selected', project);
-        }
-    })
-    .controller('AlertController', function($scope, $rootScope){
-        $rootScope.alert = {type: 'error', message: 'Go to hell!'};
-    })
-    .controller('TeamController', function ($scope){
-        $scope.team = ['abhinav', 'shelley', 'neha'];
-    })
-    .controller('HomeController', function ($scope, $rootScope, User, Task) {
-        $scope.createTask = function () {
-            var components = $scope.taskString.split('@');
-            console.log($rootScope.currentProject);
-            Task.save({
-                projectId: $rootScope.currentProject.id,
-                desc: components[0].trim()
-            }, function () {
+angular
+    .module("taskous.controllers", [])
+    .controller({
+
+        TopMenuController: function ($scope, $rootScope, Project) {
+            $scope.projects = Project.query(function () {
+                $rootScope.currentProject = $scope.projects[0];
+            });
+            $scope.selectProject = function (project) {
+                $rootScope.currentProject = project;
+                console.log('selected', project);
+            }
+        },
+
+        AlertController: function ($scope, $rootScope) {
+            $rootScope.alert = {type: 'error', message: 'Go to hell!'};
+        },
+
+        TeamController: function ($scope, $rootScope, Team) {
+
+            $scope.addUser = function () {
+                Team.save({
+                    username: $scope.newUser.username,
+                    projectId: $rootScope.currentProject.id
+                }, refreshTeam);
+            };
+
+            var refreshTeam = function () {
+                $scope.team = Team.query({
+                    projectId: $rootScope.currentProject.id
+                })
+            }
+
+            refreshTeam();
+        },
+
+        HomeController: function ($scope, $rootScope, User, Task) {
+            $scope.createTask = function () {
+                var components = $scope.taskString.split('@');
+                console.log($rootScope.currentProject);
+                Task.save({
+                    projectId: $rootScope.currentProject.id,
+                    desc: components[0].trim()
+                }, function () {
+                    $scope.tasks = Task.query({
+                        projectId: $rootScope.currentProject.id
+                    });
+                });
+            };
+            $scope.deleteTask = function (task) {
+                Task.delete({}, {
+                    id: task.id,
+                    projectId: $rootScope.currentProject.id
+                }, refreshTasks);
+            }
+            var refreshTasks = function () {
                 $scope.tasks = Task.query({
                     projectId: $rootScope.currentProject.id
                 });
-            });
-        };
-        $scope.deleteTask = function (task) {
-            Task.delete({}, {
-                id: task.id,
-                projectId: $rootScope.currentProject.id
-            }, refreshTasks);
+            };
+
+            $rootScope.$watch('currentProject', refreshTasks);
+        },
+
+        ProjectController: function ($scope, Project) {
+            $scope.createProject = function () {
+                Project.save($scope.project);
+            };
+        },
+
+        LoginController: function ($scope, $http, $location, $rootScope) {
+            $scope.user = {};
+            $scope.logIn = function () {
+                $http.post('/login', $scope.user)
+                    .success(function (user) {
+                        $rootScope.message = 'User #' + user.username + 'logged in successfully!';
+                        $rootScope.user = user.username;
+                        $location.path('/');
+                    })
+                    .error(function (err) {
+                        $rootScope.message = err.error;
+                        console.log(err);
+                    });
+            };
+
         }
-        var refreshTasks = function () {
-            $scope.tasks = Task.query({
-                projectId: $rootScope.currentProject.id
-            });
-        };
+    }
 
-        $rootScope.$watch('currentProject', refreshTasks);
-    })
-    .controller('ProjectController', function ($scope, Project) {
-        $scope.createProject = function () {
-            Project.save($scope.project);
-        };
-    })
-    .controller('LoginController', function ($scope, $http, $location, $rootScope) {
-        $scope.user = {};
-        $scope.logIn = function () {
-            $http.post('/login', $scope.user)
-                .success(function (user) {
-                    $rootScope.message = 'User #' + user.username + 'logged in successfully!';
-                    $rootScope.user = user.username;
-                    $location.path('/');
-                })
-                .error(function (err) {
-                    $rootScope.message = err.error;
-                    console.log(err);
-                });
-        };
-
-    });
+);
--- assets/js/services.js
+++ assets/js/services.js
@@ -1,46 +1,60 @@
-var taskousServices = angular.module('taskous.services', ["ngResource"]);
+angular
+    .module('taskous.services', ["ngResource"])
+    .factory(
+    {
+        User: function ($resource) {
+            return $resource(
+                "/user:userId", {
+                    userId: "@id"
+                }, {
+                    "update": {
+                        method: "PUT"
+                    },
+                    "logIn": {
+                        method: 'POST'
+                    }
 
-taskousServices.factory('User', function($resource) {
-    return $resource(
-        "/user:userId", {
-            userId: "@id"
-        }, {
-            "update": {
-                method: "PUT"
-            },
-            "logIn": {
-                method: 'POST'
-            }
-
-        }
-    );
-});
-
-taskousServices.factory('Task', function($resource) {
-    return $resource(
-        "/project/:projectId/task/:taskId", {
-            projectId: '@projectId',
-            taskId: '@id'
-        }, {}
-    );
-});
-
-taskousServices.factory('Project', function($resource) {
-    return $resource(
-        "/project/:projectId", {
-            projectId: '@id'
-        }, {}
-    );
-});
-
-taskousServices.factory('AppData', function() {
-    var data = {};
-    return {
-        get: function(key) {
-            return data[key];
+                }
+            );
         },
-        set: function(key, value) {
-            data[key] = value;
+
+        Task: function ($resource) {
+            return $resource(
+                "/project/:projectId/task/:taskId", {
+                    projectId: '@projectId',
+                    taskId: '@id'
+                }, {}
+            );
+        },
+
+        Team: function ($resource) {
+            return $resource(
+                "/project/:projectId/team/:teamId", {
+                    projectId: '@projectId',
+                    teamId: '@id'
+                }, {}
+            );
+        },
+
+        Project: function ($resource) {
+            return $resource(
+                "/project/:projectId", {
+                    projectId: '@id'
+                }, {}
+            );
+        },
+
+        AppData: function () {
+            var data = {};
+            return {
+                get: function (key) {
+                    return data[key];
+                },
+                set: function (key, value) {
+                    data[key] = value;
+                }
+            };
         }
-    };
-});
+
+    });
+
