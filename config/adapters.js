0d7ef5942c22fb684b6885bf2443d113669f7135
Kumar Abhinav <kabhinav.iitg@gmail.com>
2014-03-05 22:34:07 +0530

    xxx

--- config/adapters.js
+++ config/adapters.js
@@ -1,8 +1,8 @@
 /**
  * Global adapter config
- * 
+ *
  * The `adapters` configuration object lets you create different global "saved settings"
- * that you can mix and match in your models.  The `default` option indicates which 
+ * that you can mix and match in your models.  The `default` option indicates which
  * "saved setting" should be used if a model doesn't have an adapter specified.
  *
  * Keep in mind that options you define directly in your model definitions
@@ -14,38 +14,49 @@
 
 module.exports.adapters = {
 
-  // If you leave the adapter config unspecified 
-  // in a model definition, 'default' will be used.
-  'default': 'herokuDB',
+    // If you leave the adapter config unspecified
+    // in a model definition, 'default' will be used.
+    'default': 'herokuDB',
 
-  // Persistent adapter for DEVELOPMENT ONLY
-  // (data is preserved when the server shuts down)
-  disk: {
-    module: 'sails-disk'
-  },
+    // Persistent adapter for DEVELOPMENT ONLY
+    // (data is preserved when the server shuts down)
+    disk: {
+        module: 'sails-disk'
+    },
 
-  // MySQL is the world's most popular relational database.
-  // Learn more: http://en.wikipedia.org/wiki/MySQL
-  myLocalMySQLDatabase: {
+    // MySQL is the world's most popular relational database.
+    // Learn more: http://en.wikipedia.org/wiki/MySQL
+    myLocalMySQLDatabase: {
 
-    module: 'sails-mysql',
-    host: 'localhost',
-    user: 'root',
-    // Psst.. You can put your password in config/local.js instead
-    // so you don't inadvertently push it up if you're using version control
-    password: 'beachbody', 
-    database: 'taskous_db'
-  },
+        module: 'sails-mysql',
+        host: 'localhost',
+        user: 'root',
+        // Psst.. You can put your password in config/local.js instead
+        // so you don't inadvertently push it up if you're using version control
+        password: 'beachbody',
+        database: 'taskous_db'
+    },
 
-  herokuDB: {
-      module: 'sails-postgresql',
-      url: process.env.DATABASE_URL,
-//      module: 'sails-mysql',
-//      host: 'localhost',
-//      user: 'postgres',
-      // Psst.. You can put your password in config/local.js instead
-      // so you don't inadvertently push it up if you're using version control
-//      password: 'root',
-//      database: 'taskous_db'
-  }
-};
+    herokuDB: {
+        module: 'sails-postgresql',
+        host: process.env.PG_HOSTNAME || 'localhost',
+        user: process.env.PG_USER || 'root',
+        password: process.env.PG_PASSWORD || '',
+        database: process.env.PG_DATABASE || 'taskous_db',
+        port: process.env.PG_PORT || 5432,
+        ssl: {
+            rejectUnauthorized: false
+        }
+
+//  herokuDB: {
+//      module: 'sails-postgresql',
+//      url: process.env.DATABASE_URL,
+////      module: 'sails-mysql',
+////      host: 'localhost',
+////      user: 'postgres',
+//      // Psst.. You can put your password in config/local.js instead
+//      // so you don't inadvertently push it up if you're using version control
+////      password: 'root',
+////      database: 'taskous_db'
+//  }
+    };
